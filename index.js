f50bfcc700cd7e55aa7cbb9ef44d60aec8c450d4
Gallagher Donovan Pryor <gallagher.pryor@gmail.com>
2019-05-28 06:55:58 -0400

    drop dev code

--- index.js
+++ index.js
@@ -20,80 +20,8 @@ function lineNoToEndIdx(filePath, lineNo, cb) {
         return cb(false, globalIdx)
       }
     } while (idx !== -1)
-  }).on("end", () => {
-    // cb(false, globalIdx)
   }).on("error", () => { cb(true) })
 }
-
-function readFileToByteNo(filePath, endByte, cb) {
-  var out = Buffer.allocUnsafe(endByte + 1)
-  var fd = fs.open(filePath, 'r', (err, fd) => {
-    if (err) return cb(err)
-    fs.read(fd, out, 0, endByte + 1, 0, (err) => {
-      if (err) return cb(err)
-      cb(false, out)
-      fs.close(fd, (err) => {
-        if (err) throw(new Error('fs.close() failure'))
-      })
-    })
-  })
-}
-
-function readFileFromByteNo(filePath, begByte, cb) {
-  fs.stat(filePath, (err, stat) => {
-    if (err) cb(err)
-    var bytes = stat.size - begByte
-    var out = Buffer.allocUnsafe(bytes)
-    var fd = fs.open(filePath, 'r', (err, fd) => {
-      if (err) return cb(err)
-      fs.read(fd, out, 0, bytes, begByte, (err) => {
-        if (err) return cb(err)
-        cb(false, out)
-        fs.close(fd, (err) => {
-          if (err) throw(new Error('fs.close() failure'))
-        })
-      })
-    })
-  })
-}
-
-function head(filePath, endLine, cb) {
-  if (endLine < 1)
-    return cb(false, Buffer.allocUnsafe(0))
-  lineNoToEndIdx(filePath, endLine, function(err, byteNo) {
-    readFileToByteNo(filePath, byteNo, cb)
-  })
-}
-
-function tailFrom(filePath, startLine, cb) {
-  if (startLine <= 1)
-    return readFileFromByteNo(filePath, 0, cb)
-  lineNoToEndIdx(filePath, startLine - 1, function(err, byteNo) {
-    M(`got end idx ${byteNo}`)
-    readFileFromByteNo(filePath, byteNo + 1, cb)
-  })
-}
-
-// fn = 'test.txt'
-// tailFrom(fn, 4, (err, b) => {
-//   M(b.toString())
-//   tailFrom(fn, 0, (err, b) => {
-//     M(b.toString())
-//     tailFrom(fn, 1, (err, b) => {
-//       M(b.toString())
-//       tailFrom(fn, 20, (err, b) => {
-//         M('long tail')
-//         M(b.toString())
-//         head(fn, 0, (err, b) => {
-//           M(b.toString())
-//           head(fn, 20, (err, b) => {
-//             M(b.toString())
-//           })
-//         })
-//       })
-//     })
-//   })
-// })
 
 function readFileRange(filePath, begByte, endByte, cb) {
   var bytes = endByte - begByte + 1
